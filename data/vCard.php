2bff1aa76e62d280b9a31188a3def5f558fbb992
Matthew van Buuren <MatthewvanBuuren@Gmail.com>
2024-05-20 20:29:10 +0100

    Backup 2024

---
+++ data/vCard.php
@@ -0,0 +1,5 @@
+<?php
+header('Content-disposition: attachment; filename=Matthew William van Buuren.vcf');
+header('Content-type: text/x-vcard');
+readfile('Matthew William van Buuren.vcf');
+?>
